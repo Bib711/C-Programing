5a59f4d5f7534db4fdf620d4a61f47bd95236437
Bib711 <bibinmaleparambil111@gmail.com>
2024-02-28 22:03:52 +0530

    Q2 Done

--- tut5.c
+++ tut5.c
@@ -1,7 +1,7 @@
 //Tutorial ---> 4
 
 // Q1 eligible to vote or not?
-#include <stdio.h>
+/*#include <stdio.h>
 int main (){
     int age;
     printf("Enter your Age: "); scanf("%d", &age);
@@ -10,5 +10,20 @@ int main (){
     else
         printf("\nSorry, you are not eligible to vote yet.");        
     return 0;    
+}*/
+ 
+ //Q2 to find youngest among 3
+#include <stdio.h>
+int main (){
+    int Ram, Shyam, Ajay;
+    printf("Enter Age of Ram, Shyam, and Ajay(age1 age2 age3): "); scanf("%d%d%d", &Ram,&Shyam,&Ajay);
+    if ((Ram<=Shyam) && (Ram<=Ajay))
+       printf("The youngest is Ram(%d years).", Ram);
+    else if (Shyam<=Ram &&  Shyam<=Ajay)
+        printf("The youngest is Shyam(%d years).", Shyam);
+    else
+        printf("The youngest is Ajay(%d years).", Ajay);    
+    return 0;       
 }
+
  
