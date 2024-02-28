869a594824e822c9a9d3536f7e75e9f3a94da3c6
Bib711 <bibinmaleparambil111@gmail.com>
2024-02-28 22:08:41 +0530

    Q3 Done

--- tut5.c
+++ tut5.c
@@ -6,14 +6,14 @@ int main (){
     int age;
     printf("Enter your Age: "); scanf("%d", &age);
     if (age >=18)
-        printf("\n: Congratulations! You are eligible to cast your vote.");
+        printf("\nCongratulations! You are eligible to cast your vote.");
     else
         printf("\nSorry, you are not eligible to vote yet.");        
     return 0;    
 }*/
  
  //Q2 to find youngest among 3
-#include <stdio.h>
+/*#include <stdio.h>
 int main (){
     int Ram, Shyam, Ajay;
     printf("Enter Age of Ram, Shyam, and Ajay(age1 age2 age3): "); scanf("%d%d%d", &Ram,&Shyam,&Ajay);
@@ -24,6 +24,18 @@ int main (){
     else
         printf("The youngest is Ajay(%d years).", Ajay);    
     return 0;       
-}
+}*/
 
- 
+ //Q3 to check wheather entered no is +ve -ve or 0
+#include <stdio.h>
+int main (){
+    int num;
+    printf("Enter the number: "); scanf("%d", &num);
+    if (num >0)
+        printf("\n%d is positive.",num);
+    else if(num<0)
+        printf("\n%d is negative.",num); 
+    else
+        printf("\n%d is zero.",num);  
+    return 0;    
+}
