c063dc9d5c59a4b4b40c63e3ce68394a6e8893e6
Bib711 <bibinmaleparambil111@gmail.com>
2024-03-09 05:44:25 +0530

    Done Q1 of tut7

---
+++ tut7.c
@@ -0,0 +1,13 @@
+//Tutorial ---> 7
+
+//Q1 to print nos from 10-1
+/*#include <stdio.h>
+int main(){
+    int num = 10;
+    while (num >= 1){
+        printf("%d\n", num);
+        --num;
+    }
+    return 0;
+}*/
+
