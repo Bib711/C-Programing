d630c37f643a86832afa114b54f62da65d52b866
Bib711 <bibinmaleparambil111@gmail.com>
2024-03-26 06:25:10 +0530

    Done q4 nd finished tut 8

--- tut8.c
+++ tut8.c
@@ -33,7 +33,7 @@ return 0;
 }*/
 
 //Q3 --> print prime nos in a given range
-#include <stdio.h>
+/*#include <stdio.h>
 int main() {
     int range;
     printf("Enter the range: ");
@@ -46,6 +46,34 @@ int main() {
             }
         }
         if (flag==0)printf("%d ",i);
+    }
+return 0;
+}*/
+
+//Q4 --> another diamond pattern
+#include <stdio.h>
+int main() {
+    int rows, spaces;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (int i = 1; i <= rows; i++) {
+        // Print spaces
+        for (spaces = i; spaces < rows; spaces++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for (int i = rows; i >= 0; i--) {
+        for (spaces = i; spaces < rows; spaces++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
     }
 return 0;
 }
