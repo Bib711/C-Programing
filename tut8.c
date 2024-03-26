911eb9a478e5ec8e10fbc6a5602f83f8dee2266d
Bib711 <bibinmaleparambil111@gmail.com>
2024-03-26 06:31:14 +0530

    done some minor chnages in the last pattern question

--- tut8.c
+++ tut8.c
@@ -56,9 +56,10 @@ int main() {
     int rows, spaces;
     printf("Enter the number of rows: ");
     scanf("%d", &rows);
-    for (int i = 1; i <= rows; i++) {
-        // Print spaces
-        for (spaces = i; spaces < rows; spaces++) {
+    //upper triangle pattern
+    for (int i = 1; i <= rows+1; i++) {
+        //to print spaces
+        for (spaces = i; spaces < rows+1; spaces++) {
             printf(" ");
         }
         for (int j = 1; j <= i; j++) {
@@ -66,8 +67,10 @@ int main() {
         }
         printf("\n");
     }
+    //lower triangle pattern
     for (int i = rows; i >= 0; i--) {
-        for (spaces = i; spaces < rows; spaces++) {
+        //to print spaces
+        for (spaces = i; spaces < rows+1; spaces++) {
             printf(" ");
         }
         for (int j = 1; j <= i; j++) {
