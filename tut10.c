f179f3591ccc4e6f2740769076dcc973b18a0933
Bib711 <bibinmaleparambil111@gmail.com>
2024-05-01 18:26:01 +0530

    did tut 10

--- tut10.c
+++ tut10.c
@@ -43,7 +43,7 @@ int main() {
 }*/
 
 //to find the maximum number of characters in a string
-#include <stdio.h>
+/*#include <stdio.h>
 #include <string.h>
 
 int main() {
@@ -57,6 +57,65 @@ int main() {
     max_chars = strlen(str);
     //here -1 is to exclude \n
     printf("The maximum number of characters in the string is: %d\n", max_chars-1);
+
+    return 0;
+}*/
+
+
+//to find the number of times a given word 'the' appears in the given string.
+/*#include <stdio.h>
+
+int main() {
+    char str[100];
+    char word[4] = "the";
+    int count = 0, i, j, flag;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        flag = 1;
+        for (j = 0; word[j] != '\0'; j++) {
+            if (str[i + j] != word[j]) {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1 && (str[i + j] == ' ' || str[i + j] == '\0')) {
+            count++;
+        }
+    }
+
+    printf("The word 'the' appears %d times in the given string.\n", count);
+
+    return 0;
+}*/
+
+//to find the frequency of characters.
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int freq[26] = {0};
+    int i = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            int index = str[i] - 'a';
+            freq[index]++;
+        }
+    }
+
+    printf("Character frequencies:\n");
+    for (int j = 0; j < 26; j++) {
+        if (freq[j] > 0) {
+            printf("%c: %d\n", 'a' + j, freq[j]);
+        }
+    }
 
     return 0;
 }
