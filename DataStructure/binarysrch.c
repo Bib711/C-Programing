c4c5c2f008d97bc9279b22dfd889a92fd2d32e18
Bib711 <123845805+Bib711@users.noreply.github.com>
2024-08-01 21:51:00 +0530

    Updated bubble sort condition

--- DataStructure/binarysrch.c
+++ DataStructure/binarysrch.c
@@ -17,7 +17,7 @@ int main(){
     tc +=size+1;
     tc += (size*(size+1));
     for(int i=0; i<size; i++){
-        for(int j=0; j<size-1; j++){
+        for(int j=0; j<size-i-1; j++){
             if(arr[j] > arr[j+1]){
                 int temp = arr[j+1];
                 arr[j+1]=arr[j];
